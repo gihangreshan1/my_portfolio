30650c24c06fd9f17bd27c1d325148ee192a251a
gihangreshan1 <gihangreshan@gmail.com>
2026-07-11 14:11:25 +0530

    add volunteer section

--- script.js
+++ script.js
@@ -38,3 +38,62 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const volunteerEvents = [
+
+{
+    title:"Introduction to Machine Learning for Software Engineers organized by SESoc",
+    role:"Event Volunteer",
+    image:"images/sesoc-1.jfif",
+    date:"November 2025",
+    description:
+    "Tested the official SESoc website, identified bugs, verified features, and collaborated with the development team to improve usability."
+},
+
+{
+    title:"Anual Grand Meeting (SESoc) 2026",
+    role:"Event Volunteer",
+    image:"images/sesoc-2.jfif",
+    date:"January 2026",
+    description:
+    "Supported participant registration, technical coordination, and event logistics while assisting teams throughout the competition."
+},
+
+{
+    title:"QA Testing for SESoc  official website",
+    role:"QA Volunteer",
+    image:"images/sesoc-site.png",
+    date:"August 2025",
+    description:
+    "Guided beginners through HTML, CSS, and JavaScript fundamentals while helping them build their first responsive website."
+}
+
+];
+
+const volunteerContainer = document.getElementById("volunteerContainer");
+
+volunteerEvents.forEach(event => {
+
+    volunteerContainer.innerHTML += `
+
+    <div class="volunteer-card">
+
+        <img src="${event.image}" alt="${event.title}">
+
+        <div class="volunteer-content">
+
+            <h3>${event.title}</h3>
+
+            <div class="volunteer-role">${event.role}</div>
+
+            <p>${event.description}</p>
+
+            <span class="volunteer-date">${event.date}</span>
+
+        </div>
+
+    </div>
+
+    `;
+
+});
