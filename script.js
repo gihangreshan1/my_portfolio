2229469b7151aca70ce41c49cac10169638ac1dd
gihangreshan1 <gihangreshan@gmail.com>
2026-07-24 07:25:39 +0530

    Upadate product and certificate features

--- script.js
+++ script.js
@@ -142,3 +142,18 @@ target="_blank">
 `;
 
 });
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Close menu after clicking
+
+document.querySelectorAll(".nav-links a").forEach(link=>{
+    link.addEventListener("click",()=>{
+        navLinks.classList.remove("active");
+    });
+});
